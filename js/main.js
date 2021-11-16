b54786f7072eaf43cdd4bc0bcb86c99ea3092a51
Valentin Rodriguez <antares-alfa@hotmail.com>
2021-11-16 16:23:56 -0400

    features

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    // SCROLL
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 90) {
+            $(".navbar").addClass("navbar-shrink");
+        }else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    })
+
+    // VIDEO POPUP
+    const videoSrc = $("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click", function () {
+        if ($(".video-popup").hasClass("open")) {
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src","")
+        } else {
+            $(".video-popup").addClass("open");
+            if ($("#player-1").attr("src") == "") {
+                $("#player-1").attr("src","videoSrc")                
+            }
+        }
+    })
+
+    //CAROUSEL FEATURES
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items : 1,
+            },
+            // breakpoint from 480 up
+            600 : {
+                items : 2,
+            },
+            // breakpoint from 768 up
+            1000 : {
+                items : 3,
+            }
+        }
+    });
+})
