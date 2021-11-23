610909e25c7b993603bb77d13ef722d378124923
enm2071 <enm2071@gmail.com>
2021-11-23 02:21:36 -0400

    some customizations have been added

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             $(".navbar").addClass("navbar-shrink");
             $(".nav-link").addClass("letter-white");
             $(".navbar .nav-item i").addClass("letter-white");
-        }else{
+        } else {
             $(".navbar").removeClass("navbar-shrink");
             $(".nav-link").removeClass("letter-white");
             $(".navbar .nav-item i").removeClass("letter-white");
@@ -20,20 +20,20 @@ $(document).ready(function () {
     //         $(".dip-cert .container .row").removeClass("owl-carousel features-carousel");
     //     }
     // });
-    
+
     //CAROUSEL FEATURES
     $('.features-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive : {
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
             // breakpoint from 0 up
-            0 : {
-                items : 1,
+            0: {
+                items: 1,
             },
             // breakpoint from 480 up
-            800 : {
-                items : 2,
+            800: {
+                items: 2,
             },
             // breakpoint from 768 up
             // 1000 : {
@@ -42,11 +42,33 @@ $(document).ready(function () {
         }
     });
     var owl = $('.owl-carousel');
-    $('#customNextBtn').click(function() {
+    $('#customNextBtn').click(function () {
         owl.trigger('next.owl.carousel');
     })
 
-    $('#customNextBtn2').click(function() {
+    $('#customNextBtn2').click(function () {
         owl.trigger('next.owl.carousel');
     })
+
 })
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+document.getElementById("toggle").addEventListener("click", function () {
+
+    var x = document.getElementById("collapsibleNavbar");
+    if (x.className === "collapse navbar-collapse") {   
+        x.classList.add("show");
+    } else {
+        x.classList.remove("show");
+    }
+});
